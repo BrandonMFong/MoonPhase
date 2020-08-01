8b8efa264bc079f1e0f354e67f63f26e3281b567
BrandonMFong <fong.m.brandon97@gmail.com>
2020-07-31 22:34:37 -0700

    [Code] lessen int main and put to the functions

--- src/main.c
+++ src/main.c
@@ -1,21 +1,51 @@
-#include "moonphase.h"
+/*
+ * Is this sending serial data?
+ * Use the gpio numbers
+ */
 
-int
-main(int argc, char *argv[])
+#include "RPi_GPIO_Code_Samples.h"
+
+/*
+ * Enable GPIO pins
+ */
+void init_gpio(int pin)
+{
+	if (-1 == GPIOExport(pin)) 
+        exit(-1);
+}
+
+/*
+ * Set GPIO directions
+ */
+void set_input(int pin)
+{
+	if (-1 == GPIODirection(pin, IN))
+        exit(-1);
+}
+
+/*
+ * Set GPIO directions
+ */
+void set_output(int pin)
+{
+	if (-1 == GPIODirection(pin, OUT))
+        exit(-1);
+}
+
+/*
+ * Disable GPIO pins
+ */
+void disable_pin()
+{
+	if (-1 == GPIOUnexport(POUT) || -1 == GPIOUnexport(PIN))
+		return(4);
+}
+
+
+int main(int argc, char *argv[])
 {
 	int repeat = 10;
 
-	/*
-	 * Enable GPIO pins
-	 */
-	if (-1 == GPIOExport(POUT) || -1 == GPIOExport(PIN))
-		return(1);
-
-	/*
-	 * Set GPIO directions
-	 */
-	if (-1 == GPIODirection(POUT, OUT) || -1 == GPIODirection(PIN, IN))
-		return(2);
 
 	do {
 		/*
@@ -32,12 +62,6 @@ main(int argc, char *argv[])
 		usleep(500 * 1000);
 	}
 	while (repeat--);
-
-	/*
-	 * Disable GPIO pins
-	 */
-	if (-1 == GPIOUnexport(POUT) || -1 == GPIOUnexport(PIN))
-		return(4);
 
 	return(0);
 }
