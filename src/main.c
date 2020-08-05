abf04e6b2c2475399b8937a9fe84a48f7126cf3b
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-04 18:20:15 -0700

    [Code] Include

--- src/main.c
+++ src/main.c
@@ -3,55 +3,7 @@
 //  * Use the gpio numbers
 //  */
 
-// #include "RPi_GPIO_Code_Samples.h"
-
-// /*
-//  * Enable GPIO pins
-//  */
-// void init_gpio(int pin)
-// {
-// 	if (-1 == GPIOExport(pin)) 
-//         exit(-1);
-// }
-
-// /*
-//  * Set GPIO directions
-//  */
-// void set_input(int pin)
-// {
-// 	if (-1 == GPIODirection(pin, IN))
-//         exit(-1);
-// }
-
-// /*
-//  * Set GPIO directions
-//  */
-// void set_output(int pin)
-// {
-// 	if (-1 == GPIODirection(pin, OUT))
-//         exit(-1);
-// }
-
-// /*
-//  * Disable GPIO pins
-//  */
-// void disable_pin()
-// {
-// 	if (-1 == GPIOUnexport(POUT) || -1 == GPIOUnexport(PIN))
-// 		return(4);
-// }
-
-// /*
-//  * Write GPIO value
-//  * hmmm, should out be bool or int type?
-//  * I just don't want to hold up so much space for this variable
-//  */
-// void gpio_output(int pin, bool out)
-// {
-//     if (-1 == GPIOWrite(POUT, (int)out))
-//         return(3);
-// }
-
+#include "moonphase.h"
 
 int main(int argc, char *argv[])
 {
