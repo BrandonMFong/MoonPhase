d89384c107154237e9292bf317c0f546c22b024a
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-25 18:26:00 -0700

    making switches

--- src/main.c
+++ src/main.c
@@ -18,45 +18,38 @@
 
 unsigned char GetPortValue()
 {
-        switch(GetMoonState())
-        {
-                case NEW: return (0<<GPIO06)|(0<<GPIO13)|(0<<GPIO19)|(0<<GPIO26);
-                case WANINGCRESCENT: return (1<<GPIO06)|(0<<GPIO13)|(0<<GPIO19)|(0<<GPIO26);
-                case THIRDQTR: return (1<<GPIO06)|(1<<GPIO13)|(0<<GPIO19)|(0<<GPIO26);
-                case WANINGGIBBOUS: return (1<<GPIO06)|(1<<GPIO13)|(1<<GPIO19)|(0<<GPIO26);
-                case FULL: return (1<<GPIO06)|(1<<GPIO13)|(1<<GPIO19)|(1<<GPIO26);
-                case WAXINGGIBBOUS: return (0<<GPIO06)|(1<<GPIO13)|(1<<GPIO19)|(1<<GPIO26);
-                case FIRSTQTR: return (0<<GPIO06)|(0<<GPIO13)|(1<<GPIO19)|(1<<GPIO26);
-                case WAXINGCRESCENT: return (0<<GPIO06)|(0<<GPIO13)|(0<<GPIO19)|(1<<GPIO26);
-        }
+    switch(GetMoonState())
+    {
+        case NEW: return (0<<GPIO06)|(0<<GPIO13)|(0<<GPIO19)|(0<<GPIO26);
+        case WANINGCRESCENT: return (1<<GPIO06)|(0<<GPIO13)|(0<<GPIO19)|(0<<GPIO26);
+        case THIRDQTR: return (1<<GPIO06)|(1<<GPIO13)|(0<<GPIO19)|(0<<GPIO26);
+        case WANINGGIBBOUS: return (1<<GPIO06)|(1<<GPIO13)|(1<<GPIO19)|(0<<GPIO26);
+        case FULL: return (1<<GPIO06)|(1<<GPIO13)|(1<<GPIO19)|(1<<GPIO26);
+        case WAXINGGIBBOUS: return (0<<GPIO06)|(1<<GPIO13)|(1<<GPIO19)|(1<<GPIO26);
+        case FIRSTQTR: return (0<<GPIO06)|(0<<GPIO13)|(1<<GPIO19)|(1<<GPIO26);
+        case WAXINGCRESCENT: return (0<<GPIO06)|(0<<GPIO13)|(0<<GPIO19)|(1<<GPIO26);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-        // This is not setting the array correctly 
-        int gpioArray[4] = {GPIO06NUM, GPIO13NUM, GPIO19NUM, GPIO26NUM};
+    switch (argv)
+    {
+        "--demo":
+            // This is not setting the array correctly 
+            int gpioArray[4] = { GPIO06NUM, GPIO13NUM, GPIO19NUM, GPIO26NUM };
 
-        printf("Size of gpioArray is %d\n", sizeof(gpioArray)/sizeof(gpioArray[0]));
-        for(int i = 0; i < sizeof(gpioArray)/sizeof(gpioArray[0]); i++)
-        {
+            printf("Size of gpioArray is %d\n", sizeof(gpioArray) / sizeof(gpioArray[0]));
+            for (int i = 0; i < sizeof(gpioArray) / sizeof(gpioArray[0]); i++)
+            {
                 disable_pins(gpioArray[i]); // Reset
                 init_gpio(gpioArray[i]); // Initiliaze array
                 set_output(gpioArray[i]); // set gpio pin to output
-        }
+            }
 
-        // init_gpio(PIN);
-        // set_output(PIN);
-        // I am going to use a char
-        // I want to use 4 bits to represent the phases
-        RPIPORT = 0x0F; // Right most bits will be the bits I care about
-
-        // STEPS
-        // 1 - Get Fractional
-        // 2 - Determine which phase by fractional 
-        // 3 - Assign GPIO pins
-        int i = 0;
-        while(1)
-        {
+            int i = 0;
+            while (1)
+            {
                 // RPIPORT = GetPortValue();
 
                 // // I think this can be a thread
@@ -67,33 +60,80 @@ int main(int argc, char *argv[])
                 // Demo
                 switch (i)
                 {
-                        case 0:
-                                RPIPORT = 0x01 << 0;
-                                i = i + 1;
-                                break;
-                        
-                        case 1:
-                                RPIPORT = 0x01 << 1;
-                                i = i + 1;
-                                break;
-                        
-                        case 2:
-                                RPIPORT = 0x01 << 2;
-                                i = i + 1;
-                                break;
-                        
-                        case 3:
-                                RPIPORT = 0x01 << 3;
-                                i = 0;
-                                break;
-                        
-                        default:
-                                RPIPORT = 0x0F;
-                                break;
-                }
-                Set_Port(); 
-                sleep(10);    
-        }
+                case 0:
+                    RPIPORT = 0x01 << 0;
+                    i = i + 1;
+                    break;
 
-        return(0);
+                case 1:
+                    RPIPORT = 0x01 << 1;
+                    i = i + 1;
+                    break;
+
+                case 2:
+                    RPIPORT = 0x01 << 2;
+                    i = i + 1;
+                    break;
+
+                case 3:
+                    RPIPORT = 0x01 << 3;
+                    i = 0;
+                    break;
+
+                default:
+                    RPIPORT = 0x0F;
+                    break;
+                }
+                Set_Port();
+                sleep(10);
+            }
+            break;
+        "--disable-pins":
+            // This is not setting the array correctly 
+            int gpioArray[4] = { GPIO06NUM, GPIO13NUM, GPIO19NUM, GPIO26NUM };
+
+            printf("Size of gpioArray is %d\n", sizeof(gpioArray) / sizeof(gpioArray[0]));
+            for (int i = 0; i < sizeof(gpioArray) / sizeof(gpioArray[0]); i++)
+            {
+                disable_pins(gpioArray[i]); // Reset
+            }
+            break;
+            
+        default:
+            // This is not setting the array correctly 
+            int gpioArray[4] = { GPIO06NUM, GPIO13NUM, GPIO19NUM, GPIO26NUM };
+
+            printf("Size of gpioArray is %d\n", sizeof(gpioArray) / sizeof(gpioArray[0]));
+            for (int i = 0; i < sizeof(gpioArray) / sizeof(gpioArray[0]); i++)
+            {
+                disable_pins(gpioArray[i]); // Reset
+                init_gpio(gpioArray[i]); // Initiliaze array
+                set_output(gpioArray[i]); // set gpio pin to output
+            }
+
+            // init_gpio(PIN);
+            // set_output(PIN);
+            // I am going to use a char
+            // I want to use 4 bits to represent the phases
+            RPIPORT = 0x0F; // Right most bits will be the bits I care about
+
+            // STEPS
+            // 1 - Get Fractional
+            // 2 - Determine which phase by fractional 
+            // 3 - Assign GPIO pins
+            int i = 0;
+            while (1)
+            {
+                // RPIPORT = GetPortValue();
+
+                // // I think this can be a thread
+                Set_Port(); // Assign output pins
+
+                sleep(TWELVEHOURS);
+
+            }
+            break;
+    }
+
+    return(0);
 }
